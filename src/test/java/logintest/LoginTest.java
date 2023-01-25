c19205feb5f2c65b37e3cb96de656e96f2d4690a
Urooj Shadan <uroojshadan.me@gmail.com>
2023-01-25 15:55:45 -0600

    commit

--- src/test/java/logintest/LoginTest.java
+++ src/test/java/logintest/LoginTest.java
@@ -5,14 +5,15 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class LoginTest {
 
-	public static WebDriver driver;
+	WebDriver driver;
 	@BeforeMethod
-	public static void initDriver() {
+	public void initDriver() throws InterruptedException {
 		System.setProperty("webdriver.chrome.driver", "/Users/comet/Selenium/ChromeDriver/chromedriver");
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
@@ -20,10 +21,19 @@ public class LoginTest {
 		driver.manage().timeouts().pageLoadTimeout(5, TimeUnit.SECONDS);
 		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 		driver.get("https://techfios.com/billing/?ng=login/");
+		Thread.sleep(2000);
 		}
 	
 	@Test
-	public void test1() {
+	public void test1() throws InterruptedException {
 		driver.findElement(By.xpath("//input[@id='username']")).sendKeys("demo@techfios.com");
+		Thread.sleep(2000);
 	}
+	
+	@AfterMethod
+	public void tearDown() {
+		driver.close();
+		driver.quit();
+	}
+	
 }
